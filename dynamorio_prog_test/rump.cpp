85baf941072114173c77c045d8c648c3473b7373
Dylan Brasseur <helios77@outlook.fr>
2019-07-29 10:49:44 +0200

    Modified tests

--- dynamorio_prog_test/rump.cpp
+++ dynamorio_prog_test/rump.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <cmath>
 
-typedef float FTYPE;
+typedef double FTYPE;
 
 int main(int argc, char const *argv[])
 {
     FTYPE x = 77617.0;
     FTYPE y = 33096.0;
-    FTYPE res = 333.75*std::pow(y,6)+(x*x)*(11*(x*x)*(y*y)-std::pow(y, 6)-121*std::pow(y,4)-2)+5.5*std::pow(y,8)+x/(2*y);
+    FTYPE res = ((((333.75*y*y*y*y*y*y)+(x*x)*(((11*(x*x)*(y*y)-(y*y*y*y*y*y))-121*(y*y*y*y))-2))+5.5*(y*y*y*y*y*y*y*y))+x/(2*y));
     std::cout << std::scientific << res << std::endl;
     return 0;
 }
