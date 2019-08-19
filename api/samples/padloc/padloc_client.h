7ddc5a9b5a94e204fceb058b88c02c394236b888
Mickael <mteaudors@outlook.com>
2019-08-19 14:19:28 +0200

    Doxygen on backend.hxx and padloc_client.h

--- api/samples/padloc/padloc_client.h
+++ api/samples/padloc/padloc_client.h
@@ -16,119 +16,370 @@
 #include "padloc_operations.hpp"
 
 #ifndef MAX_OPND_SIZE_BYTES
-// operand size is maximum 512 bits (AVX512 instructions) = 64 bytes 
-#define MAX_OPND_SIZE_BYTES 64
+    /** 
+     * \def MAX_OPND_SIZE_BYTES 
+     * \brief operand maximum size
+     * \details Value is 512 bits (AVX512 instructions) = 64 bytes 
+     */
+    #define MAX_OPND_SIZE_BYTES 64
+
 #endif
 
+/** 
+ * \def PRINT_ERROR_MESSAGE
+ * \brief Assert error message
+ */
 #define PRINT_ERROR_MESSAGE(message) dr_fprintf(STDERR, "%s\n", (message));
 
 #ifdef WINDOWS
+
+/** 
+ * \def DR_REG_OP_A_ADDR
+ * \brief Register to store the address of the first operand
+ */
 #define DR_REG_OP_A_ADDR DR_REG_XCX
+
+/** 
+ * \def DR_REG_OP_B_ADDR
+ * \brief Register to store the address of the second operand
+ */
 #define DR_REG_OP_B_ADDR DR_REG_XDX
+
+/** 
+ * \def DR_REG_OP_C_ADDR
+ * \brief Register to store the address of the third operand
+ */
 #define DR_REG_OP_C_ADDR DR_REG_R8
-#define DR_REG_BASE DR_REG_XAX
+
 #elif defined(AARCH64)
+
+/** 
+ * \def DR_REG_OP_A_ADDR
+ * \brief Register to store the address of the first operand
+ */
 #define DR_REG_OP_A_ADDR DR_REG_X0
+
+/** 
+ * \def DR_REG_OP_B_ADDR
+ * \brief Register to store the address of the second operand
+ */
 #define DR_REG_OP_B_ADDR DR_REG_X1
+
+/** 
+ * \def DR_REG_OP_C_ADDR
+ * \brief Register to store the address of the third operand
+ */
 #define DR_REG_OP_C_ADDR DR_REG_X2
-#define DR_REG_RES_ADDR DR_REG_X3
+
 #else
+
+/** 
+ * \def DR_REG_OP_A_ADDR
+ * \brief Register to store the address of the first operand
+ */
 #define DR_REG_OP_A_ADDR DR_REG_XDI
+
+/** 
+ * \def DR_REG_OP_B_ADDR
+ * \brief Register to store the address of the second operand
+ */
 #define DR_REG_OP_B_ADDR DR_REG_XSI
+
+/** 
+ * \def DR_REG_OP_C_ADDR
+ * \brief Register to store the address of the third operand
+ */
 #define DR_REG_OP_C_ADDR DR_REG_XDX
-#define DR_REG_RES_ADDR DR_REG_XDI
-#define DR_REG_BASE DR_REG_XAX
+
 #endif
 
 #if defined(X86)
+
+/**
+ * \def DR_SCRATCH_REG
+ * \brief Buffer register for intermediary storage 
+ */
 #define DR_SCRATCH_REG DR_REG_XCX
 
+/**
+ * \def AVX_SUPPORTED 
+ * \brief True if the machine supports AVX instructions 
+ * \warn Supported only on X86
+ */
 #define AVX_SUPPORTED (proc_has_feature(FEATURE_AVX))
+
+/**
+ * \def AVX_512_SUPPORTED 
+ * \brief True if the machine supports AVX 512 instructions
+ * \warning Supported only on X86 
+ */
 #define AVX_512_SUPPORTED (proc_has_feature(FEATURE_AVX512F))
 
 #elif defined(AARCH64)
-#define DR_REG_MULTIPLE DR_REG_Q31
-#define DR_REG_FLOAT DR_REG_S31
-#define DR_REG_DOUBLE DR_REG_D31
 
-#define DR_BUFFER_REG DR_REG_X6
+/**
+ * \def DR_SCRATCH_REG
+ * \brief Buffer register for intermediary storage 
+ */
 #define DR_SCRATCH_REG DR_REG_X5
+
 #endif
 
+/**
+ * \def SPILL_SLOT_SCRATCH_REG
+ * \brief Index of the spill slot to store the value of DR_SCRATCH_REG
+ */
 #define SPILL_SLOT_SCRATCH_REG SPILL_SLOT_1
 
-/* INSTR */
+/**
+ * \def SRC
+ * \brief Shortcut for instr_get_src
+ */
 #define SRC(instr, n) instr_get_src((instr), (n))
+
+/**
+ * \def DST
+ * \brief Shortcut for instr_get_dst
+ */
 #define DST(instr, n) instr_get_dst((instr), (n))
 
 #if defined(X86)
+
+/**
+ * \def MOVE_FLOATING_SCALAR
+ * \brief Shortcut for INSTR_CREATE_movsd or INSTR_CREATE_movss depending on the precision.
+ * \details Insert a specific MOVE instruction for one floating element.
+ * \param is_double If true, then the instruction inserted is MOVSD, else it is MOVSS
+ * \param drcontext DynamoRIO context
+ * \param dest Destination operand (OPND type)
+ * \param srcd Source operand (OPND type) for MOVSD
+ * \param srcs  Source operand (OPND type) for MOVSS
+ */
 #define MOVE_FLOATING_SCALAR(is_double, drcontext, dest, srcd, srcs) (is_double) ? 	\
         INSTR_CREATE_movsd((drcontext), (dest), (srcd)) : 			  					\
         INSTR_CREATE_movss((drcontext), (dest), (srcs))
 
+/**
+ * \def MOVE_FLOATING_PACKED
+ * \brief Shortcut for INSTR_CREATE_vmovupd or INSTR_CREATE_movupd depending on the instruction type (AVX or SSE).
+ * \details Insert a specific MOVE instruction for an entire floating register.
+ * \param is_avx If true, then the instruction inserted is VMOVUPD, else it is MOVUPD
+ * \param drcontext DynamoRIO context
+ * \param dest Destination operand (OPND type)
+ * \param src Source operand (OPND type) 
+ */
 #define MOVE_FLOATING_PACKED(is_avx, drcontext, dest, src) (is_avx) ? 				\
         INSTR_CREATE_vmovupd((drcontext), (dest), (src)) : 								\
         INSTR_CREATE_movupd((drcontext), (dest), (src))
 #endif
 
 /* CREATE OPND */
+
+/**
+ * \def OP_REG
+ * \brief Shortcut for opnd_create_reg
+ */
 #define OP_REG(reg_id) opnd_create_reg((reg_id))
+
+/**
+ * \def OP_INT
+ * \brief Shortcut for opnd_create_immed_int
+ */
 #define OP_INT(val) opnd_create_immed_int((val), OPSZ_4)
+
+/**
+ * \def OP_INT64
+ * \brief Shortcut for opnd_create_immed_int64
+ */
 #define OP_INT64(val) opnd_create_immed_int64((val), OPSZ_8)
 
 #if defined(X86)
+
+/**
+ * \def OP_REL_ADDR
+ * \brief Shortcut for opnd_create_rel_addr
+ */
 #define OP_REL_ADDR(addr) \
         opnd_create_rel_addr((addr), OPSZ_8)
+
+/**
+ * \def OP_BASE_DISP
+ * \brief Shortcut for opnd_create_base_disp
+ */
 #define OP_BASE_DISP(base, disp, size) \
         opnd_create_base_disp((base), DR_REG_NULL, 0, (disp), (size))
+
 #elif defined(AARCH64)
+
+/**
+ * \def OP_REL_ADDR
+ * \brief Shortcut for opnd_create_rel_addr
+ */
 #define OP_REL_ADDR(addr) \
         opnd_create_rel_addr((addr), OPSZ_2)
+
+/**
+ * \def OP_BASE_DISP
+ * \brief Shortcut for opnd_create_base_disp
+ */
 #define OP_BASE_DISP(base, disp, size) \
         opnd_create_base_disp((base), DR_REG_NULL, 0, (disp), (size))
 #endif
 
 /* TESTS OPND */
+
+/**
+ * \def IS_REG
+ * \brief Shortcut for opnd_is_reg
+ */
 #define IS_REG(opnd) opnd_is_reg((opnd))
+
+/**
+ * \def OP_IS_BASE_DISP
+ * \brief Shortcut for opnd_is_base_disp
+ */
 #define OP_IS_BASE_DISP(opnd) opnd_is_base_disp((opnd))
+
+/**
+ * \def OP_IS_REL_ADDR
+ * \brief Shortcut for opnd_is_rel_addr
+ */
 #define OP_IS_REL_ADDR(opnd) opnd_is_rel_addr((opnd))
+
+/**
+ * \def OP_IS_ABS_ADDR
+ * \brief Shortcut for opnd_is_abs_addr
+ */
 #define OP_IS_ABS_ADDR(opnd) opnd_is_abs_addr((opnd))
+
+/**
+ * \def OP_IS_ADDR
+ * \brief True if the opnd is a relative or an absolute address
+ */
 #define OP_IS_ADDR(opnd) (OP_IS_REL_ADDR((opnd)) || OP_IS_ABS_ADDR((opnd)))
 
 /* TESTS REG */
+
+/**
+ * \def IS_GPR
+ * \brief Shortcut for reg_is_gpr
+ */
 #define IS_GPR(reg) reg_is_gpr((reg))
 
 #if defined(X86)
+
+/**
+ * \def IS_XMM
+ * \brief Shortcut for reg_is_strictly_xmm
+ */
 #define IS_XMM(reg) reg_is_strictly_xmm((reg))
+
+/**
+ * \def IS_YMM
+ * \brief Shortcut for reg_is_strictly_ymm
+ */
 #define IS_YMM(reg) reg_is_strictly_ymm((reg))
+
+/**
+ * \def IS_ZMM
+ * \brief Shortcut for reg_is_strictly_zmm
+ */
 #define IS_ZMM(reg) reg_is_strictly_zmm((reg))
+
 #elif defined(AARCH64)
+
+/**
+ * \def IS_SIMD
+ * \brief Shortcut for reg_is_simd
+ */
 #define IS_SIMD(reg) reg_is_simd((reg))
+
 #endif
 
 /* OPND GET VALUE */
+
+/**
+ * \def GET_REG
+ * \brief Shortcut for opnd_get_reg
+ */
 #define GET_REG(opnd) opnd_get_reg((opnd)) /* if opnd is a register */
+
+/**
+ * \def GET_ADDR
+ * \brief Shortcut for opnd_get_addr
+ */
 #define GET_ADDR(opnd) opnd_get_addr((opnd)) /*if opnd is an address */
 
 /* TLS OPERATIONS */
+
+/**
+ * \def GET_TLS
+ * \brief Shortcut for drmgr_get_tls_field
+ */
 #define GET_TLS(drcontext, tls) drmgr_get_tls_field((drcontext), (tls))
+
+/**
+ * \def SET_TLS
+ * \brief Shortcut for drmgr_set_tls_field
+ */
 #define SET_TLS(drcontext, tls, value) drmgr_set_tls_field((drcontext), (tls), (void*)(value))
+
+/**
+ * \def INSERT_READ_TLS
+ * \brief Shortcut for drmgr_insert_read_tls_field
+ */
 #define INSERT_READ_TLS(drcontext, tls, bb, instr, reg) \
         drmgr_insert_read_tls_field((drcontext), (tls), (bb), (instr), (reg))
+
+
+/**
+ * \def INSERT_WRITE_TLS
+ * \brief Shortcut for drmgr_insert_write_tls_field
+ */
 #define INSERT_WRITE_TLS(drcontext, tls, bb, instr, reg, temp_reg) \
         drmgr_insert_write_tls_field((drcontext), (tls_stack), (bb), (instr), (buffer_reg), (temp_reg));
 
 /* SIZES */
+
+/**
+ * \def REG_SIZE
+ * \brief Get a register size in bytes
+ */
 #define REG_SIZE(reg) opnd_size_in_bytes(reg_get_size((reg)))
+
+/**
+ * \def OPSZ
+ * \brief Shortcut for opnd_size_from_bytes. 
+ * \details Get an OPSZ constant corresponding to a bytes size.
+ */
 #define OPSZ(bytes) opnd_size_from_bytes((bytes))
 
-typedef byte SLOT;
-
 #if defined(X86)
+
+/** 
+ * \def NB_XMM_REG
+ * \brief The number of XMM registers on X86 only
+ */
 #define NB_XMM_REG 16
+
+/** 
+ * \def NB_YMM_REG
+ * \brief The number of YMM registers on X86 only
+ */
 #define NB_YMM_REG 16
+
+/** 
+ * \def NB_ZMM_REG
+ * \brief The number of ZMM registers on X86 only
+ */
 #define NB_ZMM_REG 32
+
 #elif defined(AARCH64)
+
+/** 
+ * \def NB_Q_REG
+ * \brief The number of Q registers on AARCH64 only
+ */
 #define NB_Q_REG 0
 #endif
 
