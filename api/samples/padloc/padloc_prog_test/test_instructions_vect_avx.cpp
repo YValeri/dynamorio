ca9ee841a1804becd41646d98db74d8a55a8e3fc
Mickael <mteaudors@outlook.com>
2019-08-20 13:50:25 +0200

    test update for scalar avx instructions

--- api/samples/padloc/padloc_prog_test/test_instructions_vect_avx.cpp
+++ api/samples/padloc/padloc_prog_test/test_instructions_vect_avx.cpp
@@ -19,7 +19,7 @@ int main(int argc, char const *argv[])
     v_src2_d_avx = _mm256_load_pd(src2_d_avx);
 
 
-    std::cout << "\n\t***** VECTORISED INSTRUCTIONS AVX only *****\n\n";
+    std::cout << "\n\t***** VECTORISED INSTRUCTIONS AVX *****\n\n";
 
     v_res_f_avx = _mm256_add_ps(v_src0_f_avx , v_src1_f_avx); _mm256_store_ps(res_f_avx , v_res_f_avx);
     v_res_d_avx = _mm256_add_pd(v_src0_d_avx , v_src1_d_avx); _mm256_store_pd(res_d_avx , v_res_d_avx);
@@ -29,12 +29,12 @@ int main(int argc, char const *argv[])
             res_f_avx[1] >= 0.108127-EPSILLON && res_f_avx[1] <= 0.108127+EPSILLON &&
             res_f_avx[2] >= -0.196688-EPSILLON && res_f_avx[2] <= -0.196688+EPSILLON &&
             res_f_avx[3] >= -1.44725-EPSILLON && res_f_avx[3] <= -1.44725+EPSILLON) 
-        std::cout << "\t\033[1;32m[ OK ]\033[0m" << std::endl; else std::cout  << "\t\033[1;31m[ KO ]\033[0m" << std::endl;
+        std::cout << "\033[1;32m[ OK ]\033[0m" << std::endl; else std::cout  << "\033[1;31m[ KO ]\033[0m" << std::endl;
     
     print<double>(res_d_avx , 2 , "VADDPD");
     if(     res_d_avx[0] >= 0.416576-EPSILLON && res_d_avx[0] <= 0.416576+EPSILLON &&
             res_d_avx[1] >= 0.731816-EPSILLON && res_d_avx[1] <= 0.731816+EPSILLON) 
-        std::cout << "\t\t\t\t\t\t\t\033[1;32m[ OK ]\033[0m" << std::endl; else std::cout  << "\t\t\t\t\t\t\t\033[1;31m[ KO ]\033[0m" << std::endl;
+        std::cout << "\033[1;32m[ OK ]\033[0m" << std::endl; else std::cout  << "\033[1;31m[ KO ]\033[0m" << std::endl;
     
 
     std::cout << std::endl;
@@ -48,12 +48,12 @@ int main(int argc, char const *argv[])
             res_f_avx[1] >= 1.73199-EPSILLON && res_f_avx[1] <= 1.73199+EPSILLON &&
             res_f_avx[2] >= 0.437793-EPSILLON && res_f_avx[2] <= 0.437793+EPSILLON &&
             res_f_avx[3] >= 3.68788-EPSILLON && res_f_avx[3] <= 3.68788+EPSILLON) 
-        std::cout << "\t\t\t\033[1;32m[ OK ]\033[0m" << std::endl; else std::cout  << "\t\t\t\033[1;31m[ KO ]\033[0m" << std::endl;
+        std::cout << "\033[1;32m[ OK ]\033[0m" << std::endl; else std::cout  << "\033[1;31m[ KO ]\033[0m" << std::endl;
     
     print<double>(res_d_avx , 2 , "VSUBPD");
     if(     res_d_avx[0] >= -0.381002-EPSILLON && res_d_avx[0] <= -0.381002+EPSILLON &&
             res_d_avx[1] >= 0.463603-EPSILLON && res_d_avx[1] <= 0.463603+EPSILLON) 
-        std::cout << "\t\t\t\t\t\t\t\033[1;32m[ OK ]\033[0m" << std::endl; else std::cout  << "\t\t\t\t\t\t\t\033[1;31m[ KO ]\033[0m" << std::endl;
+        std::cout << "\033[1;32m[ OK ]\033[0m" << std::endl; else std::cout  << "\033[1;31m[ KO ]\033[0m" << std::endl;
     std::cout << std::endl;
 
 
@@ -65,12 +65,12 @@ int main(int argc, char const *argv[])
             res_f_avx[1] >= -0.747024-EPSILLON && res_f_avx[1] <= -0.747024+EPSILLON &&
             res_f_avx[2] >= -0.0382441-EPSILLON && res_f_avx[2] <= -0.0382441+EPSILLON &&
             res_f_avx[3] >= -2.87648-EPSILLON && res_f_avx[3] <= -2.87648+EPSILLON) 
-        std::cout << "\t\033[1;32m[ OK ]\033[0m" << std::endl; else std::cout  << "\t\033[1;31m[ KO ]\033[0m" << std::endl;
+        std::cout << "\033[1;32m[ OK ]\033[0m" << std::endl; else std::cout  << "\033[1;31m[ KO ]\033[0m" << std::endl;
     
     print<double>(res_d_avx , 2 , "VMULPD");
     if(     res_d_avx[0] >= 0.00709311-EPSILLON && res_d_avx[0] <= 0.00709311+EPSILLON &&
             res_d_avx[1] >= 0.0801569-EPSILLON && res_d_avx[1] <= 0.0801569+EPSILLON) 
-        std::cout << "\t\t\t\t\t\t\t\033[1;32m[ OK ]\033[0m" << std::endl; else std::cout  << "\t\t\t\t\t\t\t\033[1;31m[ KO ]\033[0m" << std::endl;
+        std::cout << "\033[1;32m[ OK ]\033[0m" << std::endl; else std::cout  << "\033[1;31m[ KO ]\033[0m" << std::endl;
 
     std::cout << std::endl;
 
@@ -82,13 +82,13 @@ int main(int argc, char const *argv[])
             res_f_avx[1] >= -1.13317-EPSILLON && res_f_avx[1] <= -1.13317+EPSILLON &&
             res_f_avx[2] >= -0.380003-EPSILLON && res_f_avx[2] <= -0.380003+EPSILLON &&
             res_f_avx[3] >= -0.436332-EPSILLON && res_f_avx[3] <= -0.436332+EPSILLON) 
-        std::cout << "\t\033[1;32m[ OK ]\033[0m" << std::endl; else std::cout  << "\t\033[1;31m[ KO ]\033[0m" << std::endl;
+        std::cout << "\033[1;32m[ OK ]\033[0m" << std::endl; else std::cout  << "\033[1;31m[ KO ]\033[0m" << std::endl;
     
     
     print<double>(res_d_avx , 2 , "VDIVPD");
     if(     res_d_avx[0] >= 0.0446016-EPSILLON && res_d_avx[0] <= 0.0446016+EPSILLON &&
             res_d_avx[1] >= 4.45696-EPSILLON && res_d_avx[1] <= 4.45696+EPSILLON) 
-        std::cout << "\t\t\t\t\t\t\t\t\033[1;32m[ OK ]\033[0m" << std::endl; else std::cout  << "\t\t\t\t\t\t\t\t\033[1;31m[ KO ]\033[0m" << std::endl;
+        std::cout << "\033[1;32m[ OK ]\033[0m" << std::endl; else std::cout  << "\033[1;31m[ KO ]\033[0m" << std::endl;
     std::cout << std::endl;
     
     return 0;
